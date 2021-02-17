978790a85db596c8b2877f965e4a5d65da158f65
kivancfrh <32164822+kivancfrh@users.noreply.github.com>
2021-02-17 12:55:32 +0300

    17.02.2021

---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface ICarDal
+    {
+        void GetByld();
+        void GetAll();
+        void Add();
+        void Update();
+        void Delete();
+    }
+}
---
+++ DataAccess/Concrete/InMemory.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class InMemory : ICarDal
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetByld()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
