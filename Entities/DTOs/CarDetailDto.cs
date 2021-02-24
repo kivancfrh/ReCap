37c10e32b45983db1de28f7b42be7daa39b38644
H348632 <H348632@TU04LTBX1Y5Q2>
2021-02-24 20:18:47 +0300

    24.02.2021 -

--- Entities/DTOs/CarDetailDto.cs
+++ Entities/DTOs/CarDetailDto.cs
@@ -10,9 +10,10 @@ namespace Entities.DTOs
     {
         public int CarId { get; set; }
         public string BrandName { get; set; }
-        public string CountryName { get; set; }
+        public string  ModelName { get; set; }
         public string ColorName { get; set; }
-        public string Year { get; set; }
+        public string ModelYear { get; set; }
         public decimal DailyPrice { get; set; }
+        public string Description { get; set; }
     }
 }
